8b2c6a599a735e0c7292b01d1e2d4590b07d1fdb
Rachel Vaz <132902764+rvaz6@users.noreply.github.com>
2023-05-14 09:56:22 +1000

    Create event db

--- IAB207 Assessment 3/assessment3/models.py
+++ IAB207 Assessment 3/assessment3/models.py
@@ -10,7 +10,24 @@ class User(db.Model):
     comments = db.relationship('Comment', backref = 'user')
 
 
-class Event:
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80))
+    image = db.Column(db.String(400))
+    description = db.Column(db.String(200))
+    date = db.Column(db.Date(8))
+    time = db.Column(db.Time(4))
+    location = db.Column(db.String(100))
+    category = db.Column(db.String(80))
+    comments = db.relationship('Comment', backref = 'events')
+
+    def __repr__(self): 
+        return "<Name: {}>".format(self.name)
+
+
+    
+
 
     def _init_(self, name, image, description, date, time, location, category):
         self.name = name
