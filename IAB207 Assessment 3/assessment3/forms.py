33f0c2a1a5d7a9cf0a76b1845fbbb6b5dba248cd
Rachel Vaz <132902764+rvaz6@users.noreply.github.com>
2023-05-13 22:12:01 +1000

    Create event form

--- IAB207 Assessment 3/assessment3/forms.py
+++ IAB207 Assessment 3/assessment3/forms.py
@@ -1,3 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
+
+#Create new event
+class EventForm(FlaskForm):
+  name = StringField('Event Name', validators=[InputRequired()])
+  image = StringField('Cover Image', validators=[InputRequired()])
+  description = TextAreaField('Description', validators=[InputRequired()])
+  date = StringField('Event Date', validators=[InputRequired()])
+  time = StringField('Event Time', validators=[InputRequired()])
+  location = StringField('Event Location', validators=[InputRequired()])
+  category = StringField('Event Category', validators=[InputRequired()])
+  submit = SubmitField("Create")
+
+  
+
+
+#User comment
+class CommentForm(FlaskForm):
+  text = TextAreaField('Comment', [InputRequired()])
+  submit = SubmitField('Create')
