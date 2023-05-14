927563630bb49e5752a77821de2180d92817d86d
Rachel Vaz <132902764+rvaz6@users.noreply.github.com>
2023-05-14 11:52:34 +1000

    Update event form

--- IAB207 Assessment 3/assessment3/forms.py
+++ IAB207 Assessment 3/assessment3/forms.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+
+ALLOWED_FILE = {'PNG', 'JPG', 'png', 'jpg'}
 
 #Create new event
 class EventForm(FlaskForm):
   name = StringField('Event Name', validators=[InputRequired()])
-  image = StringField('Cover Image', validators=[InputRequired()])
+  image = FileField('Cover Image', validators=[FileRequired(message = 'Image cannot be empty'), FileAllowed(ALLOWED_FILE, message = 'Only supports PNG, JPG, png, jpg')])
   description = TextAreaField('Description', validators=[InputRequired()])
   date = StringField('Event Date', validators=[InputRequired()])
   time = StringField('Event Time', validators=[InputRequired()])
