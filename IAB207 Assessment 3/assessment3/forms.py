7df55a6cd051571a7a75cd247f38092728b45616
YisLA <128697670+YisLA@users.noreply.github.com>
2023-05-15 11:15:23 +1000

    Update forms.py

--- IAB207 Assessment 3/assessment3/forms.py
+++ IAB207 Assessment 3/assessment3/forms.py
@@ -1,10 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, EmailField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 from flask_wtf.file import FileRequired, FileField, FileAllowed
 
 ALLOWED_FILE = {'PNG', 'JPG', 'png', 'jpg'}
 
+class LoginForm(FlaskForm):
+    emailid=StringField("Email Address", validators=[InputRequired('Enter your Email Address')])
+    password=PasswordField("Password", validators=[InputRequired('Enter your Password')])
+    submit = SubmitField("Login")
+
+ # this is the registration form
+class RegisterForm(FlaskForm):
+    FirstName=StringField("First Name", validators=[InputRequired()])
+    LastName=StringField("Lirst Name", validators=[InputRequired()])    
+    emailid = EmailField("Email Address", validators=[Email("Please enter a valid email")])
+    #linking two fields - password should be equal to data entered in confirm
+    password=PasswordField("Password", validators=[InputRequired()])
+    confirm=PasswordField("Confirm Password", validators=[InputRequired()])
+    contactNumber = StringField("Contact Number", validators=[InputRequired()])
+    address =StringField("Address of Residence", validators=[InputRequired()])
+    #submit button
+    submit = SubmitField("Register")
+    
 #Create new event
 class EventForm(FlaskForm):
   name = StringField('Event Name', validators=[InputRequired()])
