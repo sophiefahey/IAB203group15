72d9a1a5bbd987bd82a8d1150b2e503df87b4f24
YisLA <128697670+YisLA@users.noreply.github.com>
2023-05-29 19:53:35 +1000

    Update forms.py

--- IAB207 Assessment 3/assessment3/forms.py
+++ IAB207 Assessment 3/assessment3/forms.py
@@ -7,39 +7,41 @@ ALLOWED_FILE = {'PNG', 'JPG', 'png', 'jpg'}
 
 #Create Login Form
 class LoginForm(FlaskForm):
-    emailid=StringField("Email Address", validators=[InputRequired('Enter your Email Address')])
-    password=PasswordField("Password", validators=[InputRequired('Enter your Password')])
+    emailid = StringField("Email Address", validators = [InputRequired('Enter your Email Address')])
+    password = PasswordField("Password", validators = [InputRequired('Enter your Password')])
     submit = SubmitField("Login")
 
  # Create registration form
 class RegisterForm(FlaskForm):
-    FirstName=StringField("First Name", validators=[InputRequired()])
-    LastName=StringField("Lirst Name", validators=[InputRequired()])    
-    emailid = EmailField("Email Address", validators=[Email("Please enter a valid email")])
+    FirstName = StringField("First Name", validators = [InputRequired()])
+    LastName = StringField("Lirst Name", validators = [InputRequired()])    
+    emailid = EmailField("Email Address", validators = [Email("Please enter a valid email")])
     #linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired()])
-    confirm=PasswordField("Confirm Password", validators=[InputRequired()])
-    contactNumber = StringField("Contact Number", validators=[InputRequired()])
-    address =StringField("Address of Residence", validators=[InputRequired()])
+    password = PasswordField("Password", validators = [InputRequired()])
+    confirm = PasswordField("Confirm Password", validators = [InputRequired()])
+    contactNumber = StringField("Contact Number", validators = [InputRequired()])
+    address = StringField("Address of Residence", validators = [InputRequired()])
     #submit button
     submit = SubmitField("Register")
     
 #Create new event
 class EventForm(FlaskForm):
-  name = StringField('Event Name', validators=[InputRequired()])
-  image = FileField('Cover Image', validators=[FileRequired(message = 'Image cannot be empty'), FileAllowed(ALLOWED_FILE, message = 'Only supports PNG, JPG, png, jpg')])
-  description = TextAreaField('Description', validators=[InputRequired()])
-  date = StringField('Event Date', validators=[InputRequired()])
-  time = StringField('Event Time', validators=[InputRequired()])
-  location = StringField('Event Location', validators=[InputRequired()])
-  category = StringField('Event Category', validators=[InputRequired()])
-  member = StringField('Number of Member Tickets', validators=[InputRequired()])
-  nonMember = StringField('Number of Non-Member Tickets', validators=[InputRequired()])
-  concession = StringField('Number of Concession Tickets', validators=[InputRequired()])
-  student = StringField('Number of Student Tickets', validators=[InputRequired()])
+  name = StringField('Event Name', validators = [InputRequired()])
+  image = FileField('Cover Image', validators = [FileRequired(message = 'Image cannot be empty'), FileAllowed(ALLOWED_FILE, message = 'Only supports PNG, JPG, png, jpg')])
+  description = TextAreaField('Description', validators = [InputRequired()])
+  date = StringField('Event Date', validators = [InputRequired()])
+  time = StringField('Event Time', validators = [InputRequired()])
+  location = StringField('Event Location', validators = [InputRequired()])
+  category = StringField('Event Category', validators = [InputRequired()])
+  member = StringField('Number of Member Tickets', validators = [InputRequired()])
+  nonMember = StringField('Number of Non-Member Tickets', validators = [InputRequired()])
+  concession = StringField('Number of Concession Tickets', validators = [InputRequired()])
+  student = StringField('Number of Student Tickets', validators = [InputRequired()])
   submit = SubmitField('Create')
 
 #User comment
 class CommentForm(FlaskForm):
   text = TextAreaField('Comment', [InputRequired()])
   submit = SubmitField('Create')
+    
+    
