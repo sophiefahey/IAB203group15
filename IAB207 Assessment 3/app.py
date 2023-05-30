2a14491030dc202f24a9b9086595778b08cbc00c
sophiefahey <132902766+sophiefahey@users.noreply.github.com>
2023-05-30 16:27:52 +1000

    Delete app.py

--- IAB207 Assessment 3/app.py
+++
@@ -1,8 +0,0 @@
-from assessment3 import create_app
-#^^^will default to __init___
-
-
-#this makes the interpreter run app
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
