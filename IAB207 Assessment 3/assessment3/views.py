3214a9887eb51ec3280ed08d8485259f4244a182
sophiefahey <132902766+sophiefahey@users.noreply.github.com>
2023-05-30 16:30:18 +1000

    Delete views.py

--- IAB207 Assessment 3/assessment3/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request
-from flask import session
-
-viewsbp = Blueprint('main',__name__)
-
-@viewsbp.route("/")
-def index():
-    return render_template("index.html")
-
