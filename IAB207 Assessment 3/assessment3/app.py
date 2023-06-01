5bfc2556dc452487bd7e1653a2a730d892b07f57
Ntl-CW <132917231+Ntl-CW@users.noreply.github.com>
2023-06-01 16:17:03 +1000

    Update app.py

--- IAB207 Assessment 3/assessment3/app.py
+++ IAB207 Assessment 3/assessment3/app.py
@@ -1,32 +1,43 @@
 import os
-from flask import Flask
-from flask import request
-from flask import session
-from flask import redirect
-from flask import render_template
+from flask import Flask, url_for, request, session, redirect, render_template
 from flask_wtf.csrf import CSRFProtect
-
+from werkzeug.utils import secure_filename
 from forms import RegisterForm, LoginForm, EventForm
 from datetime import datetime, time, date
-
-from models import db
-from models import Cookuser, Event
+from models import db, Cookuser, Event
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+# Set the upload folder
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+# Specify the allowed extensions for file uploads
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+# Configure database
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'rlacksdncjswoqkqhdkslek'
+
+# Initialize extensions
+db.init_app(app)
+migrate = Migrate(app, db)
+csrf = CSRFProtect(app)
 
 # Each event details
 @app.route('/event_details/<int:event_id>', methods=['GET'])
 def event_details(event_id):
     userid = session.get('userid', None)
-    event = Event.query.get(event_id)  # Retrieve the event from the database using the event_id
+    event = Event.query.get(event_id)
 
     if event:
         return render_template('event_details.html', event=event, userid=userid)
     else:
-        # Handle event not found case
         return render_template('error.html', message='Event not found')
 
-# User_booking_history
+# User booking history
 @app.route('/user_booking_history', methods=['GET', 'POST'])
 def user_booking_history():
     return render_template('user_booking_history.html')
@@ -35,25 +46,37 @@ def user_booking_history():
 @app.route('/create_event', methods=['GET', 'POST'])
 def create_event():
     userid = session.get('userid', None)
-    form = EventForm()  # Instantiate the EventForm
+    form = EventForm()
+
     if request.method == 'POST' and form.validate_on_submit():
         title = request.form.get('title')
         description = request.form.get('description')
-        date_str = request.form.get('date')  # Get the date as a string
-        time_str = request.form.get('time')  # Get the time as a string
+        date_str = request.form.get('date')
+        time_str = request.form.get('time')
         location = request.form.get('location')
 
-        event_date = datetime.combine(date.fromisoformat(date_str), time.fromisoformat(time_str))  # Combine date and time
-        event_time = time.fromisoformat(time_str)  # Convert time string to time object
+        event_date = datetime.combine(date.fromisoformat(date_str), time.fromisoformat(time_str))
+        event_time = time.fromisoformat(time_str)
 
-        event = Event(title=title, description=description, date=event_date, time=event_time, location=location)  # Create an instance of Event
-        db.session.add(event)  # Add the instance to the session
-        db.session.commit()  # Commit the changes to the database
+        # Save the uploaded image
+        image_file = request.files.get('image')
+        if image_file:
+            filename = secure_filename(image_file.filename)
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image_file.save(image_path)
+            event_image = 'uploads/' + filename  # Store the image path in the database
+            event = Event(title=title, description=description, date=event_date, time=event_time, location=location, image=event_image)
+        else:
+            event = Event(title=title, description=description, date=event_date, time=event_time, location=location)
+
+        db.session.add(event)
+        db.session.commit()
 
         return redirect('/events/')
 
     return render_template('create_event.html', form=form, userid=userid)
 
+# Events listing
 @app.route('/events/')
 def events():
     userid = session.get('userid', None)
@@ -71,16 +94,13 @@ def logout():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        # Get userid from the database
         session['userid'] = form.data.get('userid')
         return redirect('/')
-    
     return render_template('login.html', form=form)
 
 # Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    # Get register form fromRegisterForm
     form = RegisterForm()
     if form.validate_on_submit():
         cookuser = Cookuser()
@@ -88,7 +108,6 @@ def register():
         cookuser.username = form.data.get('username')
         cookuser.password = form.data.get('password')
 
-        # Send user details to the DB
         db.session.add(cookuser)
         db.session.commit()
 
@@ -97,35 +116,11 @@ def register():
         return redirect('/')
     return render_template('register.html', form=form)
 
-# Defualt link to index
+# Default index page
 @app.route('/')
 def index():
     userid = session.get('userid', None)
     return render_template('index.html', userid=userid)
 
 if __name__ == '__main__':
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    dbfile = os.path.join(basedir, 'db.sqlite')
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'rlacksdncjswoqkqhdkslek'
-
-    # CSRF setting
-    csrf = CSRFProtect()
-    csrf.init_app(app)
-    db.init_app(app)
-    db.app = app
-    # Set up the application context
-    with app.app_context():
-        db.create_all()
     app.run(host='127.0.0.1', port=5000, debug=True)
-
-# Cache control
-@app.after_request
-def add_cache_control(response):
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '0'
-    return response
